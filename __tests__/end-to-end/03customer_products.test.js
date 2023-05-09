3fe95bedea7fcc5ac9e1d8b854f5a5495a580b66
josiel <josiel.jcc@hotmail.com>
2023-05-09 15:59:23 -0300

    debug: testing req 13

--- __tests__/end-to-end/03customer_products.test.js
+++ __tests__/end-to-end/03customer_products.test.js
@@ -93,7 +93,9 @@ describe.only(requirement(13), () => {
     );
   });
 
-  test("O avaliador testará se o local storage contém um token válido", async () => {
+  test.only("O avaliador testará se o local storage contém um token válido", async () => {
+    console.log("tokenVerification", (await localStorage(page, "user")).token);
+    // console.log(typeof jwtKey);
     expect(
       !!jwt.verify((await localStorage(page, "user")).token, jwtKey)
     ).toEqual(true);
